ecc4f74231bed0d6b2c7b95f160cdc25798da2f7
Neal <darley.neal@gmail.com>
2023-08-29 17:28:21 -0600

    Adjust service rules

--- CurfewService/ShutdownService.cs
+++ CurfewService/ShutdownService.cs
@@ -1,16 +1,13 @@
 using System.Diagnostics;
 
+// TODO: Retrieve curfew settings from appsettings file
+// TODO: Allow to indicate active/inactive days as well as curfew times
+
 public class ShutdownService 
 {
     public void EvaluateShutdown() 
     {
-        var now = DateTime.Now;
-
-        // Should be configured in the appsettings. I'm just working this PoC for now.
-        bool IsAfterCurfew = (now.Hour >= 21 && now.Minute >= 30) || now.Hour >= 22;
-        bool IsBeforeWake = now.Hour < 5;
-
-        if (IsAfterCurfew || IsBeforeWake)
+        if (IsRestrictedCurfewTime())
         {
             Process.Start(new ProcessStartInfo("shutdown", "/s /t 0") 
             {
@@ -18,5 +15,18 @@ public class ShutdownService
                 UseShellExecute = false
             });
         }
+    }
+
+    private bool IsRestrictedCurfewTime() {
+        var now = DateTime.Now;
+        
+        if (now.DayOfWeek == DayOfWeek.Thursday || now.DayOfWeek == DayOfWeek.Friday) {
+            return false;
+        }
+
+        bool IsAfterCurfew = (now.Hour >= 21 && now.Minute >= 30) || now.Hour >= 22;
+        bool IsBeforeWake = now.Hour < 5;
+
+        return IsAfterCurfew || IsBeforeWake;
     }
 }
